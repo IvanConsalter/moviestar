470c3da282f0ac0cfb3c22eee0d9b8a59cd58a54
Ivan Consalter <ivanconsalter1@gmail.com>
2023-11-30 19:13:58 -0300

    login e logout

--- editprofile.php
+++ editprofile.php
@@ -1,5 +1,9 @@
 <?php
   include_once('templates/header.php');
+  include_once('dao/UserDAO.php');
+
+  $userDao = new UserDAO($conn, $BASE_URL);
+  $userData = $userDao->verifyToken(true);
 ?>
   <main>
     <div id="main-container" class="container-fluid">
--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,20 @@
 <?php
-  require_once("globals.php");
-  require_once("db.php");
-  require_once("models/Message.php");
-  require_once("dao/UserDAO.php");
+require_once("globals.php");
+require_once("db.php");
+require_once("models/Message.php");
+require_once("dao/UserDAO.php");
 
-  $message = new Message($BASE_URL);
-  $flashMessage = $message->getMessage();
+$message = new Message($BASE_URL);
+$flashMessage = $message->getMessage();
 
-  if(!empty($flashMessage["msg"])) {
-    $message->clearMessage();
-  }
-  
-  $userDao = new UserDAO($conn, $BASE_URL);
+if (!empty($flashMessage["msg"])) {
+  $message->clearMessage();
+}
 
-  $userData = $userDao->verifyToken(false);
-  
+$userDao = new UserDAO($conn, $BASE_URL);
+
+$userData = $userDao->verifyToken(false);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -51,9 +51,28 @@
       </form>
       <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar-nav">
-          <li class="nav-item">
-            <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
-          </li>
+          <?php if ($userData) : ?>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>newmovie.php" class="nav-link">
+                <i class="far fa-plus-square"></i> Incluir Filme
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>dashboard.php" class="nav-link">Meus Filmes</a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>editprofile.php" class="nav-link bold">
+                <?= $userData->name ?>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>logout.php" class="nav-link">Sair</a>
+            </li>
+          <?php else : ?>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+            </li>
+          <?php endif; ?>
         </ul>
       </div>
     </nav>
