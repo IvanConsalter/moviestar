1e620f7bb9961b706fd119a8574761476bb8bdfc
Ivan Consalter <ivanconsalter1@gmail.com>
2023-12-19 18:29:47 -0300

    getLatestMovies

--- index.php
+++ index.php
@@ -1,25 +1,35 @@
 <?php
   include_once('templates/header.php');
+
+  include_once("dao/MovieDAO.php");
+
+  // DAO dos filmes
+  $movieDao = new MovieDAO($conn, $BASE_URL);
+
+  $latestMovies = $movieDao->getLatestMovies();
+
 ?>
   <main>
   <div id="main-container" class="container-fluid">
     <h2 class="section-title">Filmes novos</h2>
     <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
     <div class="movies-container">
-      <div class="card movie-card">
-        <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/movie_cover.jpg');"></div>
+      <?php foreach($latestMovies as $movie): ?>
+        <div class="card movie-card">
+        <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>');"></div>
         <div class="card-body">
           <p class="card-rating">
             <i class="fas fa-star"></i>
             <span class="rating">9</span>
           </p>
           <h5 class="card-title">
-            <a href="#">Título do Filme</a>
+            <a href="<?= $BASE_URL ?>movie.php?id=<? $movie->id ?>"><?= $movie->title ?></a>
           </h5>
-          <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
-          <a href="#" class="btn btn-primary card-btn">Conhecer</a>
+          <a href="<?= $BASE_URL ?>movie.php?id=<? $movie->id ?>" class="btn btn-primary rate-btn">Avaliar</a>
+          <a href="<?= $BASE_URL ?>movie.php?id=<? $movie->id ?>" class="btn btn-primary card-btn">Conhecer</a>
         </div>
       </div>
+      <?php endforeach; ?>
     </div>
     <h2 class="section-title">Ação</h2>
     <p class="section-description">Veja os melhores filmes de ação</p>
