b4fc3b7abd59e8a0cc56ed11d8583a4d1139e97c
Ivan Consalter <ivanconsalter1@gmail.com>
2024-01-03 15:07:27 -0300

    get movie by id

---
+++ movie.php
@@ -0,0 +1,47 @@
+<?php
+  require_once("templates/header.php");
+
+  require_once("models/Movie.php");
+  require_once("dao/MovieDAO.php");
+
+  // Pegar o id do filme
+  $id = filter_input(INPUT_GET, "id");
+
+  $movie;
+
+  $movieDao = new MovieDAO($conn, $BASE_URL);
+
+  if(empty($id)) {
+
+    $message->setMessage("O filme não foi encontrado!", "error", "index.php");
+
+  } else {
+
+    $movie = $movieDao->findById($id);
+
+    // Verifica se o filme existe
+    if(!$movie) {
+
+      $message->setMessage("O filme não foi encontrado!", "error", "index.php");
+
+    }
+
+  }
+
+  // Checar se o filme é do usuário
+  $userOwnsMovie = false;
+
+  if(!empty($userData)) {
+
+    if($userData->id === $movie->user_id) {
+      $userOwnsMovie = true;
+    }
+
+    // Resgatar as revies do filme
+  }
+
+
+?>
+<?php
+  require_once("templates/footer.php");
+?>
